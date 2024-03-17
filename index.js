90aa5318ef6564dcd88867815b582926ab944eeb
veroniaa <154385483+veroniaa@users.noreply.github.com>
2024-03-18 00:25:26 +0200

    nn

--- index.js
+++ index.js
@@ -31,46 +31,4 @@ function showSignUp() {
 }
 
 
-const cardWrapper = document.querySelector('.card-wrapper')
-const widthToScroll = cardWrapper.children[0].offsetWidth
-const arrowPrev = document.querySelector('.arrow.prev')
-const arrowNext = document.querySelector('.arrow.next')
-const cardBounding = cardWrapper.getBoundingClientRect()
-const cardImageAndLink = cardWrapper.querySelectorAll('img, a')
-let currScroll = 0
-let initPos = 0
-let clicked = false
 
-cardImageAndLink.forEach(item=> {
-  item.setAttribute('draggable', false)
-})
-
-arrowPrev.onclick = function() {
-  cardWrapper.scrollLeft -= widthToScroll
-}
-
-arrowNext.onclick = function() {
-  cardWrapper.scrollLeft += widthToScroll
-}
-
-cardWrapper.onmousedown = function(e) {
-  cardWrapper.classList.add('grab')
-  initPos = e.clientX - cardBounding.left
-  currScroll = cardWrapper.scrollLeft
-  clicked = true
-}
-
-cardWrapper.onmousemove = function(e) {
-  if(clicked) {
-    const xPos = e.clientX - cardBounding.left
-    cardWrapper.scrollLeft = currScroll + -(xPos - initPos)
-  }
-}
-
-cardWrapper.onmouseup = mouseUpAndLeave
-cardWrapper.onmouseleave = mouseUpAndLeave
-
-function mouseUpAndLeave() {
-  cardWrapper.classList.remove('grab')
-  clicked = false
-}
